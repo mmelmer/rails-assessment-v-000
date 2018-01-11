9eaaeadc0025096bee122cadd0537e1914eeec2a
mmelmer <mikemelmer@gmail.com>
2018-01-10 21:31:22 -0500

    defined venue_params and added create action for venues

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -12,4 +12,19 @@ class VenuesController < ApplicationController
     @venue = Venue.new
   end
 
+  def create
+    @venue = Venue.new(venue_params)
+    if @venue.save
+      redirect_to venue_path(@venue)
+    else
+      render :new
+    end
+  end
+
+  private
+
+    def venue_params
+      params.require(:venue).permit(:name)
+    end
+
 end
