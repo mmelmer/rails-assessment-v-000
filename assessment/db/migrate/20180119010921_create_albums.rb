5f9c24d5a40183c7d21fd92cb172194e41810b7f
mmelmer <mikemelmer@gmail.com>
2018-01-19 20:34:44 -0500

    created join table migrations for artist_genres and album_genres; added attributes to other migrations

--- assessment/db/migrate/20180119010921_create_albums.rb
+++ assessment/db/migrate/20180119010921_create_albums.rb
@@ -1,6 +1,11 @@
 class CreateAlbums < ActiveRecord::Migration
   def change
     create_table :albums do |t|
+      t.string :name
+      t.text :review
+      t.date :release_date
+      t.integer :artist_id
+      t.integer :user_id
 
       t.timestamps null: false
     end
