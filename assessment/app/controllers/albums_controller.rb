91f1ebcd7a40d55bf481bd97880afc17fc1ab53d
mmelmer <mikemelmer@gmail.com>
2018-01-22 20:41:24 -0500

    added index and new actions to albums_controller; created album index view

--- assessment/app/controllers/albums_controller.rb
+++ assessment/app/controllers/albums_controller.rb
@@ -1,3 +1,11 @@
 class AlbumsController < ApplicationController
   
+  def index
+    @albums = Album.all
+  end
+
+  def new
+    @artist = Arist.new
+  end
+
 end
--- assessment/db/seeds.rb
+++ assessment/db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+artist1 = Artist.create(
+  name: "Singer"
+  )
+
+artist2 = Artist.create(
+  name: "Some other dude"
+  )
+
+album1 = Album.create(
+  name: "songs.",
+  artist_id: 1
+  )
+
