5fad31ffcdc72cccaec52f399dfc195de7b552aa
mmelmer <mikemelmer@gmail.com>
2018-01-22 21:22:07 -0500

    added created action for albums; defined album_params

--- assessment/app/controllers/albums_controller.rb
+++ assessment/app/controllers/albums_controller.rb
@@ -12,4 +12,21 @@ class AlbumsController < ApplicationController
     @album = Album.new
   end
 
+  def create
+    @album = Album.new(album_params)
+  end
+
+  private
+
+    def album_params
+      params.require(:album).permit(
+          :name,
+          :review,
+          :release_date,
+          genre_ids: [],
+          genres_attributes: [:name]
+          artists_attributes: [:name]
+        )
+    end
+
 end
