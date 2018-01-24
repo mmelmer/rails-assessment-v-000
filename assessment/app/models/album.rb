afc468fc4ca4be59c21a39b31f093df33acf27e9
mmelmer <mikemelmer@gmail.com>
2018-01-23 20:51:46 -0500

    added custom writers for genre and artist within albums_controller

--- assessment/app/models/album.rb
+++ assessment/app/models/album.rb
@@ -5,4 +5,25 @@ class Album < ActiveRecord::Base
   has_many :genres, through: :album_genres
 
   validates :name, presence: true
+
+
+    def artist_attributes=(artist_attributes)
+      artist_attributes.values.each do |artist_attribute|
+        if artist_attribute[:name] !1= ""
+          artist = Artist.find_or_create_by(artist_attributes)
+          self.artist = artist
+        end
+      end
+    end
+
+    def genre_attributes=(genre_attributes)
+      genre_attributes.values.each do |genre_attribute|
+        if genre_attribute[:name] !1= ""
+          genre = Genre.find_or_create_by(genre_attributes)
+          self.genres << artist
+        end
+      end
+    end
+
+
 end
