fc1c24ee9ca4168144fb4f5d6206b04ca90b3fd8
mmelmer <mikemelmer@gmail.com>
2018-01-24 19:41:04 -0500

    changed artist collection from check box to drop down in album new form

--- assessment/app/models/album.rb
+++ assessment/app/models/album.rb
@@ -7,19 +7,19 @@ class Album < ActiveRecord::Base
   validates :name, presence: true
 
 
-    def artist_attributes=(artist_attributes)
-      artist_attributes.values.each do |artist_attribute|
-        if artist_attribute[:name] !1= ""
-          artist = Artist.find_or_create_by(artist_attributes)
+    def artists_attributes=(artists_attributes)
+      artists_attributes.values.each do |artist_attribute|
+        if artist_attribute[:name] != ""
+          artist = Artist.find_or_create_by(artists_attributes)
           self.artist = artist
         end
       end
     end
 
-    def genre_attributes=(genre_attributes)
-      genre_attributes.values.each do |genre_attribute|
-        if genre_attribute[:name] !1= ""
-          genre = Genre.find_or_create_by(genre_attributes)
+    def genres_attributes=(genres_attributes)
+      genres_attributes.values.each do |genre_attribute|
+        if genre_attribute[:name] != ""
+          genre = Genre.find_or_create_by(genres_attributes)
           self.genres << artist
         end
       end
