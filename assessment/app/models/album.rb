62d21d5a238b9b25b9daa36218605d81c203876d
mmelmer <mikemelmer@gmail.com>
2018-02-02 19:12:13 -0500

    corrected genre check boxes so they show up in album create

--- assessment/app/models/album.rb
+++ assessment/app/models/album.rb
@@ -6,6 +6,8 @@ class Album < ActiveRecord::Base
 
   validates :name, presence: true
 
+  validates :artist, presence: true
+
   has_attached_file :image, styles: { small: "64x64", med: "100x100", large: "200x200" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
