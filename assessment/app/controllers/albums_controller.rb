864b3dabee6a9bad74cffd01dfc02e3f80539456
mmelmer <mikemelmer@gmail.com>
2018-02-07 20:21:39 -0500

    added show and index routes for user; corrected display of user's albums on user show page

--- assessment/app/controllers/albums_controller.rb
+++ assessment/app/controllers/albums_controller.rb
@@ -18,6 +18,7 @@ class AlbumsController < ApplicationController
 
   def create
     @album = Album.new(album_params)
+    @album.user_id = current_user.id
     if @album.save
       redirect_to album_path(@album)
     else
--- assessment/config/routes.rb
+++ assessment/config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
+  
   resources :albums
   resources :artists
   resources :genres
-
+  resources :users, only: [:show, :index]
   devise_for :users
 
-  resources :users
   root to: "albums#index"
-
 
 end
