190cc1fbf79b46f0b5f849119318f2895654f2a6
mmelmer <mikemelmer@gmail.com>
2018-02-07 21:24:47 -0500

    corrected routes for user

--- assessment/config/routes.rb
+++ assessment/config/routes.rb
@@ -2,9 +2,11 @@ Rails.application.routes.draw do
   
   resources :albums
   resources :artists
-  resources :genres
-  resources :users, only: [:show, :index]
+  resources :genres, only: [:show, :index]
   devise_for :users
+  #get '/users/sign_up' => 'devise/registrations#new'
+  #get '/users/sign_in' => 'devise/sessions#new'
+  resources :users, only: [:show, :index]
 
   root to: "albums#index"
 
