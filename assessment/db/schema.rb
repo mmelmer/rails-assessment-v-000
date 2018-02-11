3536e7524649784c1ff5065fc4f83bccf3395ced
mmelmer <mikemelmer@gmail.com>
2018-02-10 19:16:51 -0500

    added image attachment to users table

--- assessment/db/schema.rb
+++ assessment/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180210235841) do
+ActiveRecord::Schema.define(version: 20180211001520) do
 
   create_table "album_genres", force: :cascade do |t|
     t.integer "album_id"
@@ -59,6 +59,10 @@ ActiveRecord::Schema.define(version: 20180210235841) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
