da381bfa3502b73ff500c3bf463ab62913abf3cc
mmelmer <mikemelmer@gmail.com>
2018-02-12 19:50:16 -0500

    added display of user image and bio to user show page, along with additional link to post a new album if none have been posted yet.

--- assessment/app/models/user.rb
+++ assessment/app/models/user.rb
@@ -6,4 +6,7 @@ class User < ActiveRecord::Base
 
   has_many :albums
   
+  has_attached_file :image, styles: { small: "64x64", med: "100x100", large: "200x200" }
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+  
 end
