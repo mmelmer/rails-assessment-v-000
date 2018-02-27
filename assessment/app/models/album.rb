79d3c91c99d5d4c9c9269c4cd9316a08914b3141
mmelmer <mikemelmer@gmail.com>
2018-02-26 22:47:25 -0500

    changed css styling for albums index page

--- assessment/app/models/album.rb
+++ assessment/app/models/album.rb
@@ -10,35 +10,32 @@ class Album < ActiveRecord::Base
 
   accepts_nested_attributes_for :genres
 
-
   has_attached_file :image, styles: { small: "64x64", med: "100x100", large: "200x200" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
-  #scope :by_genre, ->(genre_id){ joins(:album_genres).where("album_genres.genre_id = ?", genre_id) }
   scope :most_popular, -> (limit){ order("counter desc").limit(limit)}
   scope :by_user, ->(user_id){ where("albums.user_id = ?", user_id) }
 
-
-    def artist_attributes=(artist_attributes)
-      if artist_attributes[:name] != ""
-        artist = Artist.find_or_create_by(artist_attributes)
-        self.artist = artist
-      end
+  def artist_attributes=(artist_attributes)
+    if artist_attributes[:name] != ""
+      artist = Artist.find_or_create_by(artist_attributes)
+      self.artist = artist
     end
+  end
 
-    def genre_attributes=(genre_attributes)
-      if genre_attributes[:name] != ""
-        genre = Genre.find_or_create_by(genre_attributes)
-        self.genres << genre
-        self.save
-      end
-    end
-
-    def page_count
-      self.counter +=1
+  def genre_attributes=(genre_attributes)
+    if genre_attributes[:name] != ""
+      genre = Genre.find_or_create_by(genre_attributes)
+      self.genres << genre
       self.save
-      return self.counter
     end
+  end
+
+  def page_count
+    self.counter +=1
+    self.save
+    return self.counter
+  end
 
 
 end
