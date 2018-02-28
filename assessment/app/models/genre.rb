abdcfae1b5b1b0c121b16e6af19b43275e42de09
mmelmer <mikemelmer@gmail.com>
2018-02-27 21:07:16 -0500

    removed name requirement from genre, which was preventing reviews from being created unless a new genre was named at the same time

--- assessment/app/models/genre.rb
+++ assessment/app/models/genre.rb
@@ -2,6 +2,6 @@ class Genre < ActiveRecord::Base
   has_many :album_genres
   has_many :albums, through: :album_genres
 
-  validates :name, presence: true
+  #validates :name, presence: true
   #validates :name, uniqueness: true
 end
