a33bf740f8328dcf9235e98ed97a26c4f6ac9647
mmelmer <mikemelmer@gmail.com>
2018-02-27 21:27:47 -0500

    added current_user condition before edit_album_path link is shown on album show page

--- assessment/app/models/album.rb
+++ assessment/app/models/album.rb
@@ -11,7 +11,7 @@ class Album < ActiveRecord::Base
   accepts_nested_attributes_for :genres
 
   has_attached_file :image, styles: { small: "64x64", med: "100x100", large: "200x200" },
-  :default_url => "/assets/medium/boo.jpg"
+  :default_url => "/assets/ medium/boo.jpg"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
   scope :most_popular, -> (limit){ order("counter desc").limit(limit)}
