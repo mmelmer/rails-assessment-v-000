4b1737c95d6cf7f7a369d03000522f715bc2c02a
mmelmer <mikemelmer@gmail.com>
2018-03-04 18:51:47 -0500

    removed spotify omniauth, installed facebook omniauth gem and figaro gem

--- assessment/config/initializers/devise.rb
+++ assessment/config/initializers/devise.rb
@@ -250,7 +250,7 @@ Devise.setup do |config|
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
 
 
-  #config.omniauth :spotify, ENV['CLIENT_ID'], ENV['FACEBOOK_SECRET'], scope: 'playlist-read-private user-read-private user-read-email'
+  config.omniauth :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_APP_SECRET'], scope: 'email'
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
