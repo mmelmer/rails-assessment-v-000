b29fae891b18a4a977893b3068b39aed6a0d1b4e
mmelmer <mikemelmer@gmail.com>
2018-04-23 21:22:02 -0400

    edited formatting of user show page; created new default image for user that fails to upload user image

--- assessment/app/models/user.rb
+++ assessment/app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
 
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :omniauthable, omniauth_providers: %i[facebook]
 
-  has_attached_file :image, styles: { small: "64x64", med: "100x100", large: "200x200" }, :default_url => "default_:style.jpg"
+  has_attached_file :image, styles: { small: "64x64", med: "100x100", large: "200x200" }, :default_url => ":style/blank_icon.jpg"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
   
   def self.from_omniauth(auth)
